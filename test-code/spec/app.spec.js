560251b2098ec3b8a481472c9870d416ebcacb7a
S4mu3l3P <samuelepinginf@gmail.com>
2015-12-10 09:39:31 +0100

    added testing

--- test-code/spec/app.spec.js
+++ test-code/spec/app.spec.js
@@ -1,7 +1,26 @@
-describe("App", function() {
+describe("Coatch", function () {
+
+    it("Correct initialization", function () {
+        expect(Coach.start("easy")).toBe(true);
+        expect(Coach.start("22a")).toBe(false);
+        expect(Coach.start("")).toBe(false);
+    });
+
+    it("Correct tracking", function () {
+        expect(Coach.track("walk",1)).toBe(null);
+        expect(Coach.start("easy")).toBe(true);
+        expect(Coach.track("walk",1)).toBe(50);
+        expect(Coach.track("2walk",1)).toBe(null);
+        expect(Coach.track("bike",2)).toBe(200);
+    });
     
-  it("should..", function() { 
-      pending();
-  });  
- 
+    it("Correct tracking", function () {
+        Coach.start("easys");
+        expect(Coach.checkProgress()).toBe(null);
+        Coach.start("easy");
+        Coach.track("walk",1);
+        Coach.track("bike",2);
+        expect(Coach.checkProgress()).toBe(50);
+    });
+
 });
